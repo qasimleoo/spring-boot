1b1200158391470ac17910356efc47982ed8aebe
qasim-leo <qasim@jfreaks.com>
2023-08-04 15:16:40 +0500

    CRUD API

--- src/main/java/com/amigos/Main.java
+++ src/main/java/com/amigos/Main.java
@@ -2,14 +2,15 @@ package com.amigos;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/v1/customers")
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
@@ -45,10 +46,43 @@ public class Main {
         this.customerRepository = customerRepository;
     }
 
-    @GetMapping("api/v1/customers")
+    @GetMapping
     public List<Customer> getCustomer(){
         return customerRepository.findAll();
     }
 
+    static public record NewCustomerRequest(
+            String name,
+            String email,
+            Integer age
+    ){}
 
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request){
+        Customer customer = new Customer();
+        customer.setName(request.name);
+        customer.setEmail(request.email);
+        customer.setAge(request.age);
+        customerRepository.save(customer);
+    }
+
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") Integer Id){
+        customerRepository.deleteById(Id);
+    }
+    @PutMapping("{customerId}")
+    public ResponseEntity<String> updateCustomer(@PathVariable("customerId") Integer customerId, @RequestBody NewCustomerRequest request) {
+        Optional<Customer> optionalCustomer = customerRepository.findById(customerId);
+
+        if (optionalCustomer.isPresent()) {
+            Customer customer = optionalCustomer.get();
+            customer.setName(request.name);
+            customer.setEmail(request.email);
+            customer.setAge(request.age);
+            customerRepository.save(customer);
+            return ResponseEntity.ok("Customer updated successfully");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
